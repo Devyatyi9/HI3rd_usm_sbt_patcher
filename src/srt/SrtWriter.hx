10772a446625d7c1ebc5e2ecd51f89c8928613d2
Devyatyi9 <devyatyi9@gmail.com>
2022-08-16 00:40:40 +0400

    srt to txt

--- src/srt/SrtWriter.hx
+++ src/srt/SrtWriter.hx
@@ -30,8 +30,7 @@ class SrtWriter {
 		var it = 0;
 		while (it < SrtData.length) {
 			o.writeString(Std.string(SrtData[it].timeStart) + ', ');
-			var timeEnd = SrtData[it].timeEnd - SrtData[it].timeStart;
-			o.writeString(Std.string(timeEnd) + ', ');
+			o.writeString(Std.string(SrtData[it].timeEnd) + ', ');
 			o.writeString(SrtData[it].text + '\n');
 			it++;
 		}
